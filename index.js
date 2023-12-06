5e504df833928ceabb358e511b5485f8ada7f765
Toni Mertanen <t.mertanen@hotmail.com>
2023-12-06 23:44:54 +0200

    Swapped var to let in index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.URL;
-    var qr_svg = qr.image(url);
+    let qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
     fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
+        if (err) throw err;
       console.log("The file has been saved!");
     });
   })
